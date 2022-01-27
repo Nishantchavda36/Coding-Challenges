980a562c35f6af48573d6be7cbc6f20c0886dd99
Nishant Chavda <nishantchavda36@gmail.com>
2022-01-26 22:52:08 -0500

    26.01.2022 Problem 2

---
+++ firstRecurringElement.js
@@ -0,0 +1,19 @@
+// find the first recurring element
+// Example
+//[2,1,3,4,5,1,5]//1
+//[1,2,3,5,4,5,6]//5
+
+function firstRecurringEle(arr){
+  let seen = {}
+    for(let i = 0; i<arr.length; i++){
+      let num =arr[i]
+      if(num in seen){
+        return num
+      }
+      seen[num] = i
+    }
+}
+
+
+let x = firstRecurringEle([2,1,3,4,5,1,5])
+x
