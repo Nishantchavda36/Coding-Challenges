efdf78a67795763e30b4151c7809eab89e648878
Nishant Chavda <59893217+Nishantchavda36@users.noreply.github.com>
2022-08-24 13:48:34 -0400

    Update runningSum.js

--- runningSum.js
+++ runningSum.js
@@ -12,10 +12,10 @@
 //Input: nums = [1,1,1,1,1]
 //Output: [1,2,3,4,5]
 //Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
-Example 3:
 
-Input: nums = [3,1,2,10,1]
-Output: [3,4,6,16,17]
+//Example 3:
+//Input: nums = [3,1,2,10,1]
+//Output: [3,4,6,16,17]
 
 //My Approach 
 //Sliding Window Method
